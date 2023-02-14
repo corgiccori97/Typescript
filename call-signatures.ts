40eb24e1c3c1ad8e64ebca21990a8ae382ef2714
corgiccori97 <tkdgmlgg7@gmail.com>
2023-02-14 14:37:10 +0900

    ~#3.0

---
+++ call-signatures.ts
@@ -0,0 +1,10 @@
+function add(a:number, b:number) {
+    return a + b;
+}
+
+const sub = (a:number, b:number) => a - b
+
+// call signature 만들기
+type Add = (a:number, b:number) => number
+const simpleAdd:Add = (a, b) => a + b
+// const simpleAdd:Add = (a, b) => {a + b} 에러. 애로우함수에서 {}를 사용하면 안의 값은 return값이 아니라 함수 내부 내용으로 처리되므로 void로 처리됨. 
---
+++ type3.ts
@@ -0,0 +1,10 @@
+let x:unknown;
+
+if (typeof(x) === 'number') {
+    let y = x + 1
+}
+
+// void
+function hello() {
+    console.log('aaaa')
+}
